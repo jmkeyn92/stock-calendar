ea022113d85f7721dc4afe05b28d47353f94f50a
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-08-14 21:37:44 +0900

    update

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const date = new Date();
 
+date.setMonth(5);
+
+date.setDate(1);
+// console.log(date.getDay());
+
+const monthDays = document.querySelector('.days');
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const firstDayIndex = date.getDay();
+
+// const nextFirstDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+
 const months = [
   'January',
   'February',
@@ -18,4 +32,16 @@ const months = [
 document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 document.querySelector('.date p').innerHTML = date.toDateString();
 
+let days = '';
+
+for(let x=firstDayIndex; x>0; x--) {
+  days += `<div class='prev-date'>${prevLastDay - x + 1}</div>`
+  monthDays.innerHTML = days;
+};
+
+
+for(let i=1; i<=lastDay; i++) {
+  days += `<div>${i}</div>`;
+  monthDays.innerHTML = days;
+};
 
