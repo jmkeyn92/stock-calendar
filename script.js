de44bc658c6de1b794e434005e5f14b2c14e9404
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-08-21 22:20:04 +0900

    week, day menu

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
+'use strict';
+
 const date = new Date();
-console.log(date);
+console.log(date.getDate());
+
 
 const prevBtn = document.querySelector('.fa-angle-left');
 const nextBtn = document.querySelector('.fa-angle-right');
 
-makeCalendar(date);
+const weekBtn = document.querySelector('#week');
+const dayBtn = document.querySelector('#day');
+
+makeMonthCalendar(date);
+
+weekBtn.addEventListener('click', (event) => {
+  // console.log(event.target);
+  makeWeekCalendar(date);
+});
+
+dayBtn.addEventListener('click', () => {
+  console.log(date.getDate());
+  makeDayCalendar(date);
+});
 
 prevBtn.addEventListener('click', () => {
-  newDate = new Date(date.setMonth(date.getMonth()-1, 1));
-  makeCalendar(newDate);
+  const newDate = new Date(date.setMonth(date.getMonth()-1, 1));
+  makeMonthCalendar(newDate);
 });
-
 
 nextBtn.addEventListener('click', () => {
-  newDate = new Date(date.setMonth(date.getMonth()+1, 1));
-  makeCalendar(newDate);
+  const newDate = new Date(date.setMonth(date.getMonth()+1, 1));
+  makeMonthCalendar(newDate);
 });
 
 
-function makeCalendar(newDate) {
+function makeDayCalendar(newDate) {
+  // document.querySelector('.weekdays').remove();
+  document.querySelector('.days').remove();
 
+  const theDay = document.querySelector('.weekdays');
+
+  let day = '';
+  // console.log(newDate.getDate());
+  if( newDate.getDate() === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
+    day += `<div class="day today">${newDate.getDate()}<div class="day__content"></div></div>`;
+  } else {
+    day += `<div class="day">${newDate.getDate()}<div class="day__content"></div></div>`;
+  };
+  theDay.innerHTML = day;
+};
+
+
+
+function makeWeekCalendar(newDate) {
+  const weekDays = document.querySelector('.days');
+  const firstDayWeek = newDate.getDate()-newDate.getDay()
+
+  let days = '';
+  for(let i=0; i < 7; i++) {
+    if( firstDayWeek + i === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
+      days += `<div class="week today">${firstDayWeek + i}<div class="week__content"></div></div>`;
+    } else {
+      days += `<div class="week">${firstDayWeek + i}<div class="week__content"></div></div>`;
+    };
+  };
+  weekDays.innerHTML = days;
+};
+
+
+function makeMonthCalendar(newDate) {
   const monthDays = document.querySelector('.days');
   const lastDay = new Date(newDate.getFullYear(), newDate.getMonth() + 1, 0).getDate();
   const prevLastDay = new Date(newDate.getFullYear(), newDate.getMonth(), 0).getDate();
@@ -55,18 +103,19 @@ function makeCalendar(newDate) {
 
 
   for(let i=1; i<=lastDay; i++) {
-    if( i === new Date().getDate() && newDate.getMonth() === new Date().getMonth() ) {
-      days += `<div class='today'>${i}</div>`;
+    if( i === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
+      days += `<div class="today">${i}</div>`;
     } else {
       days += `<div>${i}</div>`;
     };
   };
 
   for(let y=1; y <= 42 - firstDayIndex - lastDay; y++) {
-    days += `<div class='next-date'>${y}</div>`;
+    days += `<div class="next-date">${y}</div>`;
   };
 
   monthDays.innerHTML = days;
+  newDate.setDate(new Date().getDate());
 
 }
 
