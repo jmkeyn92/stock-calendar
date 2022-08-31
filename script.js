ac553e27fa2570d878d31aac00df5a0238bd578d
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-09-01 06:35:41 +0900

    day + json

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const date = new Date();
 console.log(date.getDate());
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const prevBtn = document.querySelector('.fa-angle-left');
 const nextBtn = document.querySelector('.fa-angle-right');
@@ -10,7 +11,29 @@ const nextBtn = document.querySelector('.fa-angle-right');
 const weekBtn = document.querySelector('#week');
 const dayBtn = document.querySelector('#day');
 
+
+
 makeMonthCalendar(date);
+// dayclickListener();
+
+function dayclickListener() {
+  const ListDayBtn = document.querySelectorAll('.daytemp');
+  // console.log(ListDayBtn)
+
+  ListDayBtn.forEach(item => {
+    item.addEventListener('click', () => {
+      console.log(item.textContent)
+      // const dateThisPageStr = document.querySelector('div p')
+      // const dateThisPage = new Date(dateThisPageStr);
+      date.setDate(item.textContent)
+      // console.log(date);
+      // console.log('--------')
+      makeDayCalendar(date)
+    });
+  });
+};
+
+
 
 weekBtn.addEventListener('click', (event) => {
   // console.log(event.target);
@@ -34,20 +57,53 @@ nextBtn.addEventListener('click', () => {
 
 
 function makeDayCalendar(newDate) {
-  // document.querySelector('.weekdays').remove();
+  document.querySelector('.date h1').innerHTML = months[newDate.getMonth()];
+  document.querySelector('.date p').innerHTML = newDate.toDateString();
   document.querySelector('.days').remove();
+  // if( document.querySelector('.days') ) {
+  //   document.querySelector('.days').remove();
+  // };
   document.querySelector('.weekdays').className = 'day--case'
   
   // document.querySelector('.weekdays').classList.add('day--case')
 
   const theDay = document.querySelector('.day--case');
+  console.log(newDate);
+  const newDateFormat = newDate.toLocaleDateString('pt-br').split('/').reverse().join('-');
+  // const newDateFormat = newDate.toISOString().split('T')[0];
+  console.log(newDateFormat)
+
+
+  let obj;
+  fetch('calendarDB.json')
+    .then(res => res.json())
+    // .then(data => obj = data)
+    .then(data => obj = data)
+    .then(() => {
+      console.log(obj);
+      console.log(obj[newDateFormat]);
+      let day = '';
+      if( newDate.getDate() === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
+        day += `<div class="day today">${JSON.stringify(obj[newDateFormat])}</div>`;
+      } else {
+        day += `<div class="day">${JSON.stringify(obj[newDateFormat]['파크시스템스'][0]).replace(/["]+/g, '')}</div>`;
+      };
+      theDay.innerHTML = day;
+    });
+    // return obj;
+
+  // const calendarDB = JSON.parse(calendarDB);
+  // print(calendarDB);
+
+  // console.log(obj);
+  // getData();
 
   let day = '';
   // console.log(newDate.getDate());
   if( newDate.getDate() === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
-    day += `<div class="day today"></div>`;
+    day += `<div class="day today">${data[newDateFormat]}</div>`;
   } else {
-    day += `<div class="day"></div>`;
+    day += `<div class="day">${data[newDateFormat]}</div>`;
   };
   theDay.innerHTML = day;
 };
@@ -67,6 +123,8 @@ function makeWeekCalendar(newDate) {
     };
   };
   weekDays.innerHTML = days;
+  // dayclick(); ???
+
 };
 
 
@@ -75,20 +133,7 @@ function makeMonthCalendar(newDate) {
   const lastDay = new Date(newDate.getFullYear(), newDate.getMonth() + 1, 0).getDate();
   const prevLastDay = new Date(newDate.getFullYear(), newDate.getMonth(), 0).getDate();
 
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+  
 
   document.querySelector('.date h1').innerHTML = months[newDate.getMonth()];
   document.querySelector('.date p').innerHTML = newDate.toDateString();
@@ -101,24 +146,25 @@ function makeMonthCalendar(newDate) {
   let days = '';
 
   for(let x=firstDayIndex; x>0; x--) {
-    days += `<div class='prev-date'>${prevLastDay - x + 1}</div>`;
+    days += `<div class='prev-date daytemp ${prevLastDay - x + 1}'>${prevLastDay - x + 1}</div>`;
   };
 
 
   for(let i=1; i<=lastDay; i++) {
     if( i === new Date().getDate() && newDate.getMonth() === new Date().getMonth() && newDate.getFullYear() === new Date().getFullYear() ) {
-      days += `<div class="today">${i}</div>`;
+      days += `<div class="today daytemp ${i}">${i}</div>`;
     } else {
-      days += `<div>${i}</div>`;
+      days += `<div class="daytemp ${i}">${i}</div>`;
     };
   };
 
   for(let y=1; y <= 42 - firstDayIndex - lastDay; y++) {
-    days += `<div class="next-date">${y}</div>`;
+    days += `<div class="next-date daytemp ${y}">${y}</div>`;
   };
 
   monthDays.innerHTML = days;
   newDate.setDate(new Date().getDate());
+  dayclickListener();
 
 }
 
