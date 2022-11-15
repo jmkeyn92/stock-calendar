0f5b635470c7932861dd00af2af264e69c8a54de
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-11-15 15:27:01 +0900

    u

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 'use strict';
 
-console.log('a')
+// console.log('a')
 
-const arr = [1, 2, 3];
-arr.forEach(console.log); 
+// const arr = [1, 2, 3];
+// arr.forEach(console.log); 
+
+
+let score = 80;
+console.log(score);
+// score = 80;
+
+console.log(score);
